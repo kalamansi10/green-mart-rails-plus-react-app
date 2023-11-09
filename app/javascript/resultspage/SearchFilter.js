7b27c93c48ecd34408cbfd79695614f62feff137
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-11-09 23:40:22 +0800

    Made filters select only one option per group

--- app/javascript/resultspage/SearchFilter.js
+++ app/javascript/resultspage/SearchFilter.js
@@ -18,25 +18,28 @@ export default function SearchFilter({keyword, queryString, setQueryString}) {
     let name = `filter[${filter}]`
     return options[filter].map(option => 
       <span key={option}>
-        <input type='radio' name={name} id={option} value={option} onClick={applyFilter} active='false'/>
+        <input type='checkbox' name={name} id={option} onClick={applyFilter} value={option} />
         <label htmlFor={option}>{option}</label>
       </ span>
     )
   }
 
   function applyFilter(e) {
-    console.log(e.target.checked)
-    if (e.target.active == true) {
-      e.target.active = false
-      e.target.checked = false
-      url.delete(e.target.name)
-    } else {
-      e.target.active = true
-      url.delete(e.target.name)
+    url.delete(e.target.name)
+    if (e.target.checked == true) {
+      clearGroup(e.target.name)
+      e.target.checked = true
       url.append(e.target.name, e.target.value)
     }
     setQueryString('?' + url.toString())
   }
+
+  function clearGroup(name) {
+    document.querySelectorAll(`input[name="${name}"]`).forEach(option => {
+      option.checked = false
+    })
+  }
+
 
   return (
     <>
